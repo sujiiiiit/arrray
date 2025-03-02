8a065afe7efb4a821057a0cacb631dca05ecbe89
Sujit Dwivedi <dwiwedisv@rknec.edu>
2025-03-02 22:29:30 +0530

    fix/ui auth

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,8 +1,6 @@
 import { Metadata } from "next";
 import Link from "next/link";
 import LoginForm from "@/components/login-form";
-import Typing from "@/components/typing";
-import { FlickeringGrid } from "@/components/magicui/flickering-grid";
 export const metadata: Metadata = {
   title: "Authentication",
   description: "Authentication forms built using the components.",
@@ -12,38 +10,8 @@ export default function AuthenticationPage() {
   return (
     <>
       <div className="container relative flex h-dvh flex-col items-center justify-center px-4 md:grid lg:max-w-none lg:grid-cols-2 lg:px-0 overflow-hidden ">
-        <div className="relative hidden h-full flex-col  p-10 text-white lg:flex">
-          <div className="relative z-20 flex items-center text-lg font-medium">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              strokeWidth="2"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              className="mr-2 h-6 w-6"
-            >
-              <path d="M15 6v12a3 3 0 1 0 3-3H6a3 3 0 1 0 3 3V6a3 3 0 1 0-3 3h12a3 3 0 1 0-3-3" />
-            </svg>
-            Acme Inc
-          </div>
-          
-
-          <div className="relative z-20 mt-auto">
-            <blockquote className="space-y-2">
-              <p className="text-lg">
-                <Typing
-                  sentence="&ldquo;This library has saved me countless hours of work and
-                helped me deliver stunning designs to my clients faster than
-                ever before.&rdquo;"
-                  typingSpeed={50}
-                />
-              </p>
-            </blockquote>
-          </div>
-        </div>
-        <div className="lg:p-8">
+       
+        <div className="lg:p-8ss w-dvw m-auto">
           <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
             <div className="flex flex-col space-y-2 text-center">
               <h1 className="text-2xl font-semibold tracking-tight">
