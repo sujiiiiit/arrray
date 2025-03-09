dded488e4864c2d25c2bc49d17e9153e08a96490
Sujit Dwivedi <dwiwedisv@rknec.edu>
2025-03-09 15:27:06 +0530

    fix/ui/tooltip

--- components/ui/tooltip.tsx
+++ components/ui/tooltip.tsx
@@ -1,34 +1,75 @@
-"use client"
+"use client";
 
-import * as React from "react"
-import * as TooltipPrimitive from "@radix-ui/react-tooltip"
+import * as React from "react";
+import * as TooltipPrimitive from "@radix-ui/react-tooltip";
+import { cva, type VariantProps } from "class-variance-authority";
 
-import { cn } from "@/lib/utils"
+import { cn } from "@/lib/utils";
+const TooltipProvider = TooltipPrimitive.Provider;
 
-const TooltipProvider = TooltipPrimitive.Provider
+const Tooltip = TooltipPrimitive.Root;
 
-const Tooltip = TooltipPrimitive.Root
+const TooltipTrigger = TooltipPrimitive.Trigger;
 
-const TooltipTrigger = TooltipPrimitive.Trigger
+const TooltipArrow = TooltipPrimitive.Arrow;
 
-const TooltipArrow = TooltipPrimitive.Arrow
+const tooltipContentVariants = cva(
+  "z-50 overflow-hidden border text-sm animate-in fade-in-0 zoom-in-95 data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=closed]:zoom-out-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2",
+  {
+    variants: {
+      variant: {
+        default: "border-light bg-primary text-primary-foreground",
+        secondary: "border-light bg-secondary text-secondary-foreground",
+        dark: "border-dark bg-background text-foreground",
+        destructive:
+          "border-destructive bg-destructive text-destructive-foreground",
+        outline: "border-input bg-background text-foreground shadow-sm",
+      },
+      size: {
+        default: "px-3 py-1 ",
+        sm: "px-2 py-1 text-xs",
+        lg: "px-4 py-2",
+      },
+      shape: {
+        default: "rounded-full",
+        md: "rounded-md",
+        square: "rounded-none",
+      },
+    },
+    defaultVariants: {
+      variant: "default",
+      size: "default",
+      shape: "default",
+    },
+  }
+);
+
+export interface TooltipContentProps
+  extends React.ComponentPropsWithoutRef<typeof TooltipPrimitive.Content>,
+    VariantProps<typeof tooltipContentVariants> {}
 
 const TooltipContent = React.forwardRef<
   React.ElementRef<typeof TooltipPrimitive.Content>,
-  React.ComponentPropsWithoutRef<typeof TooltipPrimitive.Content>
->(({ className, sideOffset = 4, ...props }, ref) => (
-  // <TooltipPrimitive.Portal>
+  TooltipContentProps
+>(({ className, variant, size, shape, sideOffset = 4, ...props }, ref) => (
+  <TooltipPrimitive.Portal>
     <TooltipPrimitive.Content
       ref={ref}
       sideOffset={sideOffset}
       className={cn(
-        "z-50 overflow-hidden rounded-full border border-light bg-primary px-3 py-1.5 text-[13px] text-primary-foreground animate-in fade-in-0 zoom-in-95 data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=closed]:zoom-out-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2",
-        className
+        tooltipContentVariants({ variant, size, shape, className })
       )}
       {...props}
     />
-  // </TooltipPrimitive.Portal>
-))
-TooltipContent.displayName = TooltipPrimitive.Content.displayName
+  </TooltipPrimitive.Portal>
+));
+TooltipContent.displayName = TooltipPrimitive.Content.displayName;
 
-export { Tooltip, TooltipTrigger, TooltipContent, TooltipProvider,TooltipArrow }
+export {
+  TooltipProvider,
+  Tooltip,
+  TooltipArrow,
+  TooltipTrigger,
+  TooltipContent,
+  tooltipContentVariants,
+};
